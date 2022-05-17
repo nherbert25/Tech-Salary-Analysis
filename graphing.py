48bae866af3802e0cc15f0bba132bbf9df7e49b3
Taylor Treece <taylor.d.treece@gmail.com>
2022-05-16 22:54:58 -0500

    Filtering data out that is irrelevant. Working through removal of non-US data.

--- graphing.py
+++ graphing.py
@@ -3,14 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-Report_Card = pd.read_csv("Levels_Fyi_Salary_Data.csv")
+data = pd.read_csv("Levels_Fyi_Salary_Data.csv")
+
+# Feature Engineering - Remove Outlier and Irrelevant Data
+# Drop data where Years of Experienced == 0 (Unsure if actually zero, or no answer was given)
+data = data.drop(data[data.yearsofexperience == 0.0].index, inplace=False)
+data = data.reset_index()
+
+# Drop data that is not from the US
+non_us = 0
+for i in range(len(data)):
+    if len(data['location'][i].split(',')) > 2:
+        non_us += 1
+
+
+def is_in_us(index):
+    return len(data['location'][index].split(','))
+
+
 # graph salary vs years of experience
-
-
 def moneyplz():
     # x array
-    x = Report_Card.loc[:, "yearsofexperience"]
-    y = Report_Card.loc[:, "totalyearlycompensation"]
+    x = data.loc[:, "yearsofexperience"]
+    y = data.loc[:, "totalyearlycompensation"]
 
     fig1, ax3 = plt.subplots()
     ax3.scatter(x, y)
