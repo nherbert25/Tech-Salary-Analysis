2282aefaaca21de9740e73d64062f02d5f031ad8
dojiherbs <nateherbert@gmail.com>
2022-05-22 13:37:28 -0500

    rename titanic directory

--- Titanic_Datasets/titanic_main.py
+++ Titanic_Datasets/titanic_main.py
@@ -9,20 +9,20 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 # Input data files are available in the read-only "../input/" directory
 # For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
-import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+# import os
+# for dirname, _, filenames in os.walk('/kaggle/input'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
 
 # You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All"
 # You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 
 
-train_data = pd.read_csv("/kaggle/input/titanic/train.csv")
+train_data = pd.read_csv("train.csv")
 train_data.head()
 
 
-test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
+test_data = pd.read_csv("test.csv")
 test_data.head()
 
 
